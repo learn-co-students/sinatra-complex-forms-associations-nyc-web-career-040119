1fd74512c0d79fdb659c5638884d66762f8af931
Edgar Ong <edgar@minus.com>
2019-04-24 08:48:53 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,54 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  # CREATE
+  get '/pets/new' do
+    # reference for selecting owner when creating a new pet
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    # creates a new pet and associates an existing owner (FAILED - 7)
+    @pet = Pet.create(params["pet"])
+    # creates a new pet and a new owner (FAILED - 8)
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  # SHOW
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  # UPDATE
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :"/pets/edit"
+  end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+
+    # Update pet
+    @pet.update(params["pet"])
+
+
+    # Check if new owner was created, if so, set it to the new owner
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      # Requires .save method because I am setting the new owner to one thing
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
